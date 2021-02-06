3dea4a306d2a7df4d14dfca10b4a847d968b0a0b
Dulatto <dulat.omarov@gmail.com>
2021-02-05 23:10:18 -0500

    style adjust

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -4,7 +4,7 @@ import './app-header.css';
 const AppHeader = ({toDo, done}) => {
     return (
       <div className="app-header d-flex">
-        <h1>Todo List</h1>
+        <h1 className="font-weight-bold">Todo List</h1>
         <h2>{toDo} more to do, {done} done</h2>
       </div>
     );
